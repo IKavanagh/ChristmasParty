38016adf3e7ff6244f2dbe731375067fe1f3f4dc
Ian Kavanagh <ian.kavanagh@azyra.com>
2022-11-07 21:53:06 +0000

    Set Android navigation bar colour

--- Christmas/Platforms/Android/MainActivity.cs
+++ Christmas/Platforms/Android/MainActivity.cs
@@ -7,4 +7,11 @@ namespace Christmas;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        // Set navigation bar color
+        Window.SetNavigationBarColor(Android.Graphics.Color.ParseColor("#4F74B8"));
+    }
 }
