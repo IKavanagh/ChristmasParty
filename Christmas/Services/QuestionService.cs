a1a21e46f67eeee627ccd62c9b0b262f7a8bba4b
Ian Kavanagh <ian.kavanagh@azyra.com>
2022-11-13 00:21:10 +0000

    Add new questions and answers

--- Christmas/Services/QuestionService.cs
+++ Christmas/Services/QuestionService.cs
@@ -14,6 +14,18 @@ public class QuestionService
                 Title = "What is the dress code?",
                 Answer = "The dress code for the Christmas Conference is business casual. The dress code for the Christmas Party is smart."
             },
+            new Question
+            {
+                Id = Guid.NewGuid(),
+                Title = "Where does the Azyra Christmas Conference take place?",
+                Answer = "It takes place at the Herbert Park Hotel, Ballsbridge, Dublin 4."
+            },
+            new Question
+            {
+                Id = Guid.NewGuid(),
+                Title = "Where does the Azyra Christmas Party take place?",
+                Answer = "It takes place at the Roly's Bistro, Ballsbridge, Dublin 4. It is a 2 minute walk from the Herbert Park Hotel."
+            },
         };
     } 
 }
